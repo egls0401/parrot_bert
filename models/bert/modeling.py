ae950c06494477c1d518c216db290204428017b1
egls0401 <egls0401@gmail.com>
2020-05-11 02:04:34 +0900

    training까지 완료

--- models/bert/modeling.py
+++ models/bert/modeling.py
@@ -360,11 +360,13 @@ def dropout(input_tensor, dropout_prob):
   return output
 
 
+# def layer_norm(input_tensor, name=None):
+#   x = tf.keras.layers.Flatten(input_tensor)
+#   return tf.keras.layers.LayerNormalization()(x)
+
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  # return tf.contrib.layers.layer_norm(
-      # inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
-  return tf.keras.layers.LayerNormalization(input_shape=input_tensor.shape)
+  return tf.keras.layers.LayerNormalization()(input_tensor)
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
   """Runs layer normalization followed by dropout."""
